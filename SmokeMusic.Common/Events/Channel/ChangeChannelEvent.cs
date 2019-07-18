06975d61d12ebbd2990b108c261c5af09c6e492e
zhangshu <920622327@qq.com>
2019-07-18 16:13:54 +0800

    删除文件夹

--- SmokeMusic.Common/Events/Channel/ChangeChannelEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Practices.Prism.Events;
-
-namespace SmokeMusic.Common.Events.Channel
-{
-    public class ChangeChannelEvent : CompositePresentationEvent<Logic.Models.Channel>
-    {
-
-    }
-}
